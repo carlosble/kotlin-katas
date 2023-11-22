d9a207380ef3321fe7922b16e6999dbbdf2cc63c
carlosble <carlos@leanmind.es>
2023-11-22 20:33:57 +0000

    inline var

--- src/main/kotlin/dev/kata/wordWrap/ApplicationService.kt
+++ src/main/kotlin/dev/kata/wordWrap/ApplicationService.kt
@@ -4,18 +4,18 @@ import java.io.PrintStream
 
 class ApplicationService(private val outputPrinter: PrintStream, private val errorPrinter: PrintStream) {
     fun wrap(args: Array<String>) {
-        if (args.size != 2){
+        if (args.size != 2) {
             errorPrinter.println("Wrong number of arguments. Usage: wrap text columnWidth")
             return
         }
         val columnWidth = args[1].toIntOrNull()
-        if (columnWidth == null){
+        if (columnWidth == null) {
             errorPrinter.println("Wrong argument. Column with must be an integer")
             return
         }
         val text = args[0]
-        val  eitherWrap = Wrapper.wrap(text, columnWidth)
-        eitherWrap
+
+        Wrapper.wrap(text, columnWidth)
             .map { w -> outputPrinter.println(w.toString()) }
             .mapLeft { e -> errorPrinter.println(e.message) }
     }
